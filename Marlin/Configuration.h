d16df826eba9aaa372019807dbd28fb357495e33
Maxime Barbier <maxime.barbier1991@gmail.com>
2021-01-21 13:27:39 +0100

    change heated bed temperature confuguration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -702,9 +702,14 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   //Anycubic i3 Mega Ultrabase (0.9Ω @ 22°C)
-  #define DEFAULT_bedKp 251.78
-  #define DEFAULT_bedKi 49.57
-  #define DEFAULT_bedKd 319.73
+  // #define DEFAULT_bedKp 251.78
+  // #define DEFAULT_bedKi 49.57
+  // #define DEFAULT_bedKd 319.73
+
+  //Anycubic i3 Mega Ultrabase + MK4
+  #define DEFAULT_bedKp 68.67
+  #define DEFAULT_bedKi 12.33
+  #define DEFAULT_bedKd 255.02
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
